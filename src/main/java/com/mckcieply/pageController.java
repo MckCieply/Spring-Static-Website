d58bf4893806911e14523acc289915e03152627f
Alex <mwppl123@gmail.com>
2023-07-19 12:35:05 +0200

    first crude template usage

---
+++ src/main/java/com/mckcieply/pageController.java
@@ -0,0 +1,11 @@
+package com.mckcieply;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class pageController {
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+}
