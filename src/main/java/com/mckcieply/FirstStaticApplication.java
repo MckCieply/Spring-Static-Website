b15054126b51cebe89dfa4133de8d64ee1bfca51
Alex <mwppl123@gmail.com>
2023-07-19 12:34:44 +0200

    Reduced only to serve as project starter

--- src/main/java/com/mckcieply/FirstStaticApplication.java
+++ src/main/java/com/mckcieply/FirstStaticApplication.java
@@ -3,18 +3,15 @@ package com.mckcieply;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class FirstStaticApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstStaticApplication.class, args);
-	}
-	@GetMapping("/")
-	public String landingPage(){
-		return "Welcome on landing page";
 	}
 
 }
