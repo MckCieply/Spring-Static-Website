0ad05d23d5bb340975724ccbc4263ea3cf125cfa
Alex <mwppl123@gmail.com>
2023-07-27 15:01:22 +0200

    Connecting db

--- src/main/java/com/mckcieply/ContactForm.java
+++ src/main/java/com/mckcieply/ContactForm.java
@@ -1,13 +1,18 @@
 package com.mckcieply;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name="contact")
 public class ContactForm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     @NotNull
     @Size(min=9, max=12)
     private int number;
-
     @NotNull
     private String email;
 
--- src/main/java/com/mckcieply/PageController.java
+++ src/main/java/com/mckcieply/PageController.java
@@ -1,12 +1,20 @@
 package com.mckcieply;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
+    private final ContactRepository contactRepository;
+
+    public PageController(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
     @RequestMapping(value = "/")
     public String index() {
         return "index";
@@ -26,6 +34,16 @@ public class PageController {
     @PostMapping("/index")
     public String leaveContact(@ModelAttribute ContactForm contactForm){
         System.out.println(contactForm.toString());
+
         return "index";
+    }
+
+    @RequestMapping(value ="/allContacts")
+    public String contacts(){
+        return "contacts";
+    }
+    @RequestMapping(value ="/temp")
+    public ResponseEntity getAllContacts() {
+        return ResponseEntity.ok(this.contactRepository.findAll());
     }
 }
