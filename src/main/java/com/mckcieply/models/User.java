e30068a081b9558702beb3bac79b0974cbb4f1c4
Alex <mwppl123@gmail.com>
2023-08-03 17:40:13 +0200

    Creating Model for Auth

---
+++ src/main/java/com/mckcieply/models/User.java
@@ -0,0 +1,30 @@
+package com.mckcieply.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+}
