1304b52afb4f1110e1c809415ae02577ecee043f
Alex <mwppl123@gmail.com>
2023-08-09 21:02:30 +0200

    Extracting allClaims from jwt

--- src/main/java/com/mckcieply/config/JwtService.java
+++ src/main/java/com/mckcieply/config/JwtService.java
@@ -1,11 +1,31 @@
 package com.mckcieply.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    @Value("${secret.key}")
+    private static String SECRET_KEY;
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
 
     }
 }
