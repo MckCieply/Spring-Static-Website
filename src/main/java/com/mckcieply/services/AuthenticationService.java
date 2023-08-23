8db34d3edd52620f07279cd91c4c67c1ce117923
Alex <mwppl123@gmail.com>
2023-08-23 10:46:23 +0200

    saving new user to database

--- src/main/java/com/mckcieply/services/AuthenticationService.java
+++ src/main/java/com/mckcieply/services/AuthenticationService.java
@@ -4,16 +4,32 @@ package com.mckcieply.services;
 import com.mckcieply.auth.AuthenticationResponse;
 import com.mckcieply.auth.LoginRequest;
 import com.mckcieply.auth.RegisterRequest;
+import com.mckcieply.config.JwtService;
+import com.mckcieply.models.Role;
+import com.mckcieply.models.User;
+import com.mckcieply.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
     }
 
     public AuthenticationResponse login(LoginRequest request) {
